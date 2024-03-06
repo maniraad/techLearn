ac068e5905bddd6bd7f74463f4c34e30fcc04ba2
maniraad <maniraad7@gmail.com>
2024-03-06 12:14:40 +0330

    Shared To All Page Header Responsive Shared Functions

--- frontend/public/js/shared.js
+++
@@ -1,7 +0,0 @@
-import { showUserNameInNavbar, headerResponsive } from "./funcs/shared.js";
-
-headerResponsive()
-
-window.addEventListener('load', () => {
-    showUserNameInNavbar()
-});
--- frontend/public/js/style.js
+++ frontend/public/js/style.js
@@ -1,26 +1,7 @@
-const $ = document;
+import { showUserNameInNavbar, headerResponsive } from "./funcs/shared.js";
 
-const menuNavElem = $.querySelector('#nav-bar-icon');
-const navigation = $.querySelector('.navigation');
-const navigationCloseBtn = $.querySelector('.navigation__close-btn');
-const overlay = $.querySelector('.overlay');
+headerResponsive()
 
-// Open Navigation
-menuNavElem.addEventListener('click', () => {
-    navigation.classList.remove('-right-64');
-    navigation.classList.add('right-0');
-    overlay.classList.remove('opacity-0');
-    overlay.classList.remove('invisible');
-});
-
-// Close Navigation
-navigationCloseBtn.addEventListener('click', () => {
-    navigation.classList.remove('right-0');
-    navigation.classList.add('-right-64');
-        overlay.classList.add('opacity-0');
-});
-overlay.addEventListener('click', () => {
-    navigation.classList.remove('right-0');
-    navigation.classList.add('-right-64');
-        overlay.classList.add('invisible');
+window.addEventListener('load', () => {
+    showUserNameInNavbar()
 });
