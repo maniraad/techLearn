882ce856688b9f5211913f0625a005bb9223f808
maniraad <maniraad7@gmail.com>
2024-03-06 11:22:05 +0330

    Dynamiced the User Login Btn

--- frontend/public/js/funcs/shared.js
+++ frontend/public/js/funcs/shared.js
@@ -2,9 +2,24 @@ import { getMe } from "./auth.js";
 import { isLogin } from "./utils.js";
 
 const showUserNameInNavbar = () => {
-    const userInfos = getMe().then(data => {
-        console.log(data);
-    })
+
+    const userLoginBtn = document.querySelector('#user-login-btn');
+    const userLoginBtnIcon = document.querySelector('#user-login-btn-icon');
+
+    const isUserLogin = isLogin()
+
+    if (isUserLogin) {
+        
+        const userInfos = getMe().then(data => {
+            userLoginBtnIcon.classList.add("hidden");
+            userLoginBtn.setAttribute('href', 'login.html')
+            userLoginBtn.innerHTML = data.name
+        });
+
+    } else {
+        userLoginBtn.setAttribute('href', 'login.html')
+        userLoginBtn.innerHTML = 'ورود یا ثبت نام'
+    }
 };
 
 export { showUserNameInNavbar }
