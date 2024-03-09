bc8d43094f5aa1385db3e8e945d25a404a993658
maniraad <maniraad7@gmail.com>
2024-03-09 14:19:09 +0330

    Add Onclick For SortBox

--- frontend/public/js/category.js
+++ frontend/public/js/category.js
@@ -1,8 +1,20 @@
 import { getAndShowCategoryCourses } from "./funcs/shared.js";
 
+const sort = document.querySelector('#sort');
+const sortBox = document.querySelector('#sort-box');
+const Grouping = document.querySelector('#Grouping');
+const GroupingBox = document.querySelector('#Grouping-box');
+
+sort.addEventListener('click', () => {
+    sortBox.classList.toggle('hide');
+});
+
+Grouping.addEventListener('click', () => {
+    GroupingBox.classList.toggle('hide');
+});
+
 window.addEventListener('load', () => {
-    getAndShowCategoryCourses().then(data=>{
+    getAndShowCategoryCourses().then(data => {
         console.log(data);
-    })
-    
+    });
 });
