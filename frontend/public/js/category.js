2d5495df2698cb7f3a8fce9af66eab0d1e27c6c1
maniraad <maniraad7@gmail.com>
2024-03-11 16:47:38 +0330

    Code User Sort

--- frontend/public/js/category.js
+++ frontend/public/js/category.js
@@ -1,71 +1,27 @@
-import { getAndShowCategoryCourses } from "./funcs/shared.js";
+import { getAndShowCategoryCourses, insertCourseBoxHtmlTemplate, coursesSorting } from "./funcs/shared.js";
 
 const sort = document.querySelector('#sort');
 const sortBox = document.querySelector('#sort-box');
 const Grouping = document.querySelector('#Grouping');
 const GroupingBox = document.querySelector('#Grouping-box');
 
-// sort.addEventListener('click', () => {
-//     sortBox.classList.toggle('hide');
-// });
-
 Grouping.addEventListener('click', () => {
     GroupingBox.classList.toggle('hide');
 });
 
-window.addEventListener('load', () => {
-    getAndShowCategoryCourses().then(res => {
+window.addEventListener("load", () => {
+    getAndShowCategoryCourses().then(responseCourses => {
 
+        let courses = [...responseCourses];
         const categoryCourseWrapper = document.querySelector('#category-course-wrapper');
         const sortList = document.querySelectorAll('.sort-list')
 
-        let courses = [...res]
-
+        // Show Category Courses By row showType
         if (courses.length) {
-            courses.forEach(course => {
-
-                categoryCourseWrapper.insertAdjacentHTML('beforeend',
-                    `
-                                <div
-                                    class="inline-flex flex-col items-center gap-y-4 py-3 px-4 bg-white max-w-[340px] rounded-3xl shadow-sm">
-                                    <!-- Image -->
-                                    <div class="">
-                                        <img src="images/courses/C-sharp.png" alt="Js-course" class="">
-                                    </div>
-                                    <!-- Box Body -->
-                                    <div class="flex flex-col justify-center items-start self-baseline gap-y-4">
-                                        <!-- Topic -->
-                                        <h4
-                                            class="text-2xl text-right font-EstedadMedium font-bold overflow-hidden line-clamp-1">
-                                            ${course.name}</h4>
-                                        <!-- Teacher & Time -->
-                                        <div class="flex flex-wrap text-sm">
-                                            <div class="flex-center gap-x-1 py-1 px-2">
-                                                <svg class="w-4 h-4">
-                                                    <use href="#user"></use>
-                                                </svg>
-                                                <a href="#">${course.creator}</a>
-                                            </div>
-                                            <div class="flex-center gap-x-1 py-1 px-2">
-                                                <svg class="w-4 h-4">
-                                                    <use href="#clock"></use>
-                                                </svg>
-                                                <span>۲۰:۴۳</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- Price -->
-                                    <div class="self-end text-2xl text-right font-EstedadMedium font-bold">
-                                    ${course.price === 0 ? "رایگان" : course.price.toLocaleString() + "تومان"} 
-                                    </div>
-                                    <!-- Button -->
-                                    <a href="#"
-                                        class="self-stretch py-3 text-white text-center font-EstedadMedium bg-teal-600 hover:bg-teal-700 rounded-2xl delay-100 transition-all">
-                                        مشاهده و ثبت نام </a>
-                                </div>
-                `
-                )
-            });
+            insertCourseBoxHtmlTemplate(
+                courses,
+                categoryCourseWrapper
+            );
         } else {
             categoryCourseWrapper.insertAdjacentHTML('afterend',
                 `
@@ -79,11 +35,17 @@ window.addEventListener('load', () => {
             )
         }
 
+        // Show Category Courses By user filtering method
         sortList.forEach(item => {
-            item.addEventListener('click', (event) => {
+            item.addEventListener("click", event => {
 
                 sortList.forEach(selectionItem => selectionItem.classList.remove('font-bold'));
-                event.target.classList.add('font-bold'); 
+                event.target.classList.add('font-bold');
+
+                let userFilteringSelection = event.target.dataset.key
+                let showCourses = coursesSorting([...courses], userFilteringSelection)
+
+                insertCourseBoxHtmlTemplate(showCourses, categoryCourseWrapper)
             })
         });
     });
--- frontend/public/js/funcs/shared.js
+++ frontend/public/js/funcs/shared.js
@@ -249,5 +249,85 @@ const getAndShowCategoryCourses = async () => {
     return courses
 };
 
+const insertCourseBoxHtmlTemplate = (courses, parentElement) => {
+    parentElement.innerHTML = "";
 
-export { showUserNameInNavbar, headerResponsive, getAndShowAllCourses, getAndShowPreSellCourses, getAndShowArticles, getAndShowMenus, getAndShowCategoryCourses };
+    courses.forEach(course => {
+
+        parentElement.insertAdjacentHTML('beforeend',
+            `
+                        <div
+                            class="inline-flex flex-col items-center gap-y-4 py-3 px-4 bg-white max-w-[340px] rounded-3xl shadow-sm">
+                            <!-- Image -->
+                            <div class="">
+                                <img src="images/courses/C-sharp.png" alt="Js-course" class="">
+                            </div>
+                            <!-- Box Body -->
+                            <div class="flex flex-col justify-center items-start self-baseline gap-y-4">
+                                <!-- Topic -->
+                                <h4
+                                    class="text-2xl text-right font-EstedadMedium font-bold overflow-hidden line-clamp-1">
+                                    ${course.name}</h4>
+                                <!-- Teacher & Time -->
+                                <div class="flex flex-wrap text-sm">
+                                    <div class="flex-center gap-x-1 py-1 px-2">
+                                        <svg class="w-4 h-4">
+                                            <use href="#user"></use>
+                                        </svg>
+                                        <a href="#">${course.creator}</a>
+                                    </div>
+                                    <div class="flex-center gap-x-1 py-1 px-2">
+                                        <svg class="w-4 h-4">
+                                            <use href="#clock"></use>
+                                        </svg>
+                                        <span>۲۰:۴۳</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Price -->
+                            <div class="self-end text-2xl text-right font-EstedadMedium font-bold">
+                            ${course.price === 0 ? "رایگان" : course.price.toLocaleString() + "تومان"} 
+                            </div>
+                            <!-- Button -->
+                            <a href="#"
+                                class="self-stretch py-3 text-white text-center font-EstedadMedium bg-teal-600 hover:bg-teal-700 rounded-2xl delay-100 transition-all">
+                                مشاهده و ثبت نام </a>
+                        </div>
+        `
+        )
+    });
+};
+
+const coursesSorting = (array, filterMethod) => {
+    let outPutArray = []
+
+    switch (filterMethod) {
+        case 'free': {
+            outPutArray = array.filter(course => course.price === 0)
+            break
+          }
+          case 'money': {
+            outPutArray = array.filter(course => course.price !== 0)
+            break
+          }
+          case 'first': {
+            outPutArray = [...array].reverse()
+            break
+          }
+          case 'last': {
+            outPutArray = array
+            break
+          }
+          case 'default': {
+            outPutArray = array
+            break
+          }
+          default: {
+            outPutArray = array
+          }
+    };
+
+    return outPutArray
+};
+
+export { showUserNameInNavbar, headerResponsive, getAndShowAllCourses, getAndShowPreSellCourses, getAndShowArticles, getAndShowMenus, getAndShowCategoryCourses, insertCourseBoxHtmlTemplate, coursesSorting };
