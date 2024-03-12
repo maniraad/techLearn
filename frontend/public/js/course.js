0ff1bbdb2aa6bf00a5a51a46fad3cde935abd893
maniraad <maniraad7@gmail.com>
2024-03-12 11:37:10 +0330

    Intersection Observer Scroll

--- frontend/public/js/course.js
+++ frontend/public/js/course.js
@@ -4,6 +4,50 @@ const topicLesson = document.querySelector('.topic__name');
 const topicBody = document.querySelector('.topic__body');
 const topicName = document.querySelector('.topic__name');
 const topicNameArrow = document.querySelector('.topic__name--arrow');
+const selectSection = document.querySelectorAll('.select-section');
+const menuItems = document.querySelectorAll('.menu__item');
+
+// Intersection Observer Scroll
+const observer = new IntersectionObserver(observerHandler, { threshold: 0.7 });
+function observerHandler(allSections) {
+    allSections.map(section => {
+        let sectionClassName = section.target.id;
+        let sectionMenuItem = document.querySelector(`.menu__item[data-section=${sectionClassName}]`);
+        // if (section.isIntersecting) {
+        //     sectionMenuItem.classList.add("menu-item-active");
+        // } else {
+        //     sectionMenuItem.classList.remove("menu-item-active");
+        // }
+    })
+};
+selectSection.forEach(section => {
+    observer.observe(section);
+});
+menuItems.forEach(item => {
+    item.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(`.menu-item-active`).classList.remove("menu-item-active");
+        item.classList.add("menu-item-active");
+
+        let sectionClass = item.getAttribute("data-section");
+        let sectionOffsetTop = document.querySelector(`#${sectionClass}`).offsetTop;
+
+        window.scrollTo({
+            top: sectionOffsetTop - 130,
+            behavior: "smooth"
+        });
+    })
+});
+
+
+
+
+
+
+
+
+
+
 
 topicLesson.addEventListener('click', () => {
     topicName.classList.toggle('topic__name--active');
