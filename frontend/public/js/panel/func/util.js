1e3e615aafc9346cd960d951f63bd844bc0b04e8
maniraad <maniraad7@gmail.com>
2024-03-18 16:29:46 +0330

    Get Admin Infos

---
+++ frontend/public/js/panel/func/util.js
@@ -0,0 +1,13 @@
+import { getToken } from "../../funcs/utils.js"
+
+const getAdminInfos = async () => {
+    const res = await fetch(`http://localhost:4000/v1/auth/me`, {
+        headers: {
+            "Authorization": `Bearer ${getToken()}`,
+        }
+    });
+    const data = await res.json();
+    return data
+};
+
+export { getAdminInfos }
--- frontend/public/js/panel/style.js
+++ frontend/public/js/panel/style.js
@@ -1,16 +1,34 @@
-const profileMenu = document.querySelector('.profile-menu');
-const profile = document.querySelector('.profile');
-const profileIconArrow = document.querySelector('.profile-icon');
-const asideElem = document.querySelector('aside');
-const mainElem = document.querySelector('main');
-const menuButton = document.querySelector('.menu-btn');
+import { getAdminInfos } from "./func/util.js";
 
-profile.addEventListener('click', () => {
-    profileMenu.classList.toggle('profile-menu--show');
-    profileIconArrow.classList.toggle('rotate-180')
-});
+window.addEventListener('load', () => {
 
-menuButton.addEventListener('click', () => {
-    asideElem.classList.toggle('!-right-64');
-    mainElem.classList.toggle('!mr-0');
+    // Select Element From Dom
+    const profileMenu = document.querySelector('.profile-menu');
+    const profile = document.querySelector('.profile');
+    const profileIconArrow = document.querySelector('.profile-icon');
+    const asideElem = document.querySelector('aside');
+    const mainElem = document.querySelector('main');
+    const menuButton = document.querySelector('.menu-btn');
+    const $ = document;
+    const adminNameElem = $.querySelector('.admin-name');
+    const adminWelcomeElem = $.querySelector('.admin-welcome-name');
+
+    // Handle Profile Toggle
+    profile.addEventListener('click', () => {
+        profileMenu.classList.toggle('profile-menu--show');
+        profileIconArrow.classList.toggle('rotate-180')
+    });
+
+    // Handle Menu Button
+    menuButton.addEventListener('click', () => {
+        asideElem.classList.toggle('!-right-64');
+        mainElem.classList.toggle('!mr-0');
+    });
+
+    // Admin Info
+    getAdminInfos().then(admin => {
+        console.log(admin);
+        adminNameElem.innerHTML = admin.name
+        adminWelcomeElem.innerHTML = `خوش آمدید , ${admin.name}`
+    });
 });
