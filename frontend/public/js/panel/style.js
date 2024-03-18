296c3e554fbfd698ba370c32363ab44fe8627c62
maniraad <maniraad7@gmail.com>
2024-03-18 19:09:49 +0330

    Handle notification Toggle

--- frontend/public/js/panel/style.js
+++ frontend/public/js/panel/style.js
@@ -3,9 +3,11 @@ import { getAdminInfos } from "./func/util.js";
 window.addEventListener('load', () => {
     // Select Element From Dom
     const $ = document;
-    const profileMenu = $.querySelector('.profile-menu');
-    const profile = $.querySelector('.profile');
-    const profileIconArrow = $.querySelector('.profile-icon');
+    const profileMenuElem = $.querySelector('.profile-menu');
+    const notificationMenuElem = $.querySelector('.notification-menu');
+    const notificationIconElem = $.querySelector('.notification-icon');
+    const profileElem = $.querySelector('.profile');
+    const profileIconArrowElem = $.querySelector('.profile-icon');
     const asideElem = $.querySelector('aside');
     const mainElem = $.querySelector('main');
     const menuButton = $.querySelector('.menu-btn');
@@ -14,9 +16,14 @@ window.addEventListener('load', () => {
     const adminImageElem = $.querySelector('.admin-img');
 
     // Handle Profile Toggle
-    profile.addEventListener('click', () => {
-        profileMenu.classList.toggle('profile-menu--show');
-        profileIconArrow.classList.toggle('rotate-180')
+    profileElem.addEventListener('click', () => {
+        profileMenuElem.classList.toggle('profile-menu--show');
+        profileIconArrowElem.classList.toggle('rotate-180')
+    });
+    
+    // Handle notification Toggle
+    notificationIconElem.addEventListener('click', () => {
+        notificationMenuElem.classList.toggle('profile-menu--show');
     });
 
     // Handle Menu Button
@@ -33,7 +40,7 @@ window.addEventListener('load', () => {
             // Show Admin Name
             adminNameElem.innerHTML = admin.name
             adminWelcomeElem.innerHTML = `خوش آمدید , ${admin.name}`
-            adminImageElem.setAttribute('src',`http://localhost:4000/${admin.profile}`)
+            adminImageElem.setAttribute('src', `http://localhost:4000/${admin.profile}`)
 
         } else {
             location.replace("../login.html")
