bd62782625d18f727552494d0c20c15d6244c12e
maniraad <maniraad7@gmail.com>
2024-03-18 13:06:31 +0330

    Create Courses HTML

--- frontend/public/js/panel/style.js
+++ frontend/public/js/panel/style.js
@@ -1,14 +1,16 @@
 const profileMenu = document.querySelector('.profile-menu');
 const profile = document.querySelector('.profile');
+const profileIconArrow = document.querySelector('.profile-icon');
 const asideElem = document.querySelector('aside');
 const mainElem = document.querySelector('main');
 const menuButton = document.querySelector('.menu-btn');
 
 profile.addEventListener('click', () => {
-    profileMenu.classList.toggle('profile-menu--show')
+    profileMenu.classList.toggle('profile-menu--show');
+    profileIconArrow.classList.toggle('rotate-180')
 });
 
 menuButton.addEventListener('click', () => {
     asideElem.classList.toggle('!-right-64');
-    mainElem.classList.toggle('!mr-0')
+    mainElem.classList.toggle('!mr-0');
 });
