a63008faba6e3e9455cfd96cf20ea3229dedd88a
maniraad <maniraad7@gmail.com>
2024-03-18 21:54:54 +0330

    Handle No Notification

--- frontend/public/js/panel/func/shared.js
+++ frontend/public/js/panel/func/shared.js
@@ -31,7 +31,7 @@ const insertNotificationHTMLTemplate = (notifications) => {
                 `)
         });
     } else {
-
+        notificationHeaderElem.innerHTML = 'اعلانی وجود ندارد'
     }
 };
 
