dcb1b2279297a12059af501203b331ffa43739bd
maniraad <maniraad7@gmail.com>
2024-03-20 00:40:00 +0330

    Add Textarea To Form Create Course

--- frontend/public/js/panel/func/shared.js
+++ frontend/public/js/panel/func/shared.js
@@ -1,5 +1,7 @@
 import { getToken } from "../../funcs/utils.js";
 
+let categoryID = null
+
 const insertNotificationHTMLTemplate = (notifications) => {
 
     const notificationsWrapperElem = document.querySelector('.notifications-wrapper');
@@ -61,11 +63,66 @@ const removeNotification = (notifications, notificationID) => {
 };
 
 const getAllCourses = async () => {
+    const coursesWrapper = document.querySelector('#courses-wrapper');
+
     const res = await fetch(`http://localhost:4000/v1/courses`)
 
     const courses = await res.json();
 
-    return courses
+    courses.forEach((course, index) => {
+        coursesWrapper.insertAdjacentHTML("beforeend", `
+                             <tr
+                                 class="bg-white border-b hover:bg-gray-50">
+                                 <td class="w-4 p-4">
+                                     <div class="flex items-center">
+                                         <input id="checkbox-table-1" type="checkbox"
+                                             class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2 cursor-pointer">
+                                         <label for="checkbox-table-1" class="sr-only">checkbox</label>
+                                     </div>
+                                 </td>
+                                 <th scope="row"
+                                     class="px-6 py-4 text-nowrap font-medium text-gray-900 whitespace-nowrap">
+                                     ${index + 1}
+                                 </th>
+                                 <td class="px-6 py-4 text-nowrap">
+                                     ${course.name}
+                                 </td>
+                                 <td class="px-6 py-4 text-nowrap">
+                                 ${course.price === 0 ? "رایگان" : course.price.toLocaleString() + ` <span class="text-base">تومان</span>`}
+                                 </td>
+                                 <td class="px-6 py-4 text-nowrap">
+                                     ${course.registers}
+                                 </td>
+                                 <td class="px-6 py-4 text-nowrap">
+                                     ${course.categoryID.title}
+                                 </td>
+                                 <td class="px-6 py-4 text-nowrap">
+                                    ${course.isComplete === 0 ? "درحال برگذاری" : "تکمیل شده"}
+                                 </td>
+                                 <td class="px-6 py-4 text-nowrap">
+                                     <a href="#"
+                                         class="font-medium text-blue-600 dark:text-blue-500 hover:underline">ویرایش</a>
+                                 </td>
+                                 <td class="px-6 py-4">
+                                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">حذف</a>
+                                 </td>
+                             </tr>`)
+    });
 };
 
-export { insertNotificationHTMLTemplate, seenNotification, getAllCourses };
+const getAndShowCategoryOption = async () => {
+    const categoryListElem = document.querySelector('.category-list');
+
+    const res = await fetch(`http://localhost:4000/v1/category`)
+
+    const categories = await res.json()
+
+    categories.forEach(category => categoryListElem.insertAdjacentHTML("beforeend", `<option value="${category._id}" class="text-gray-700">${category.title}</option>`));
+
+    categoryListElem.addEventListener('change', (event) => categoryID = event.target.value);
+};
+
+const createNewCourse = async () => {
+};
+
+export { insertNotificationHTMLTemplate, seenNotification, getAllCourses, createNewCourse };
