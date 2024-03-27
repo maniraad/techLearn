4ac35ed8004068d3429c4665791e8b1695ff4d7b
maniraad <maniraad7@gmail.com>
2024-03-27 16:28:42 +0330

    Start uPanel

---
+++ frontend/public/js/UPanel/style.js
@@ -0,0 +1,28 @@
+import { getAdminInfos, logout } from "./func/utils.js";
+// import { insertNotificationHTMLTemplate, seenNotification } from "./func/shared.js";
+
+window.addEventListener('load', () => {
+    // Select Element From Dom
+    const $ = document;
+    const profileMenuElem = $.querySelector('.profile-menu');
+    const profileElem = $.querySelector('.profile');
+    const profileIconArrowElem = $.querySelector('.profile-icon');
+    const asideElem = $.querySelector('aside');
+    const mainElem = $.querySelector('main');
+    const menuButton = $.querySelector('.menu-btn');
+    const adminNameElems = $.querySelectorAll('.admin-name');
+    const adminImageElem = $.querySelector('.admin-img');
+    const adminEmailElem = $.querySelector('.admin-email');
+    const logoutBtnElem = $.querySelector('.logout-btn');
+
+    // Handle Menu Button
+    menuButton.addEventListener('click', () => {
+        asideElem.classList.toggle('!-right-0');
+        mainElem.classList.toggle('md:!mr-64');
+    });
+
+    // Handle Logout Admin
+    logoutBtnElem.addEventListener("click", () => {
+        logout()
+    })
+});
