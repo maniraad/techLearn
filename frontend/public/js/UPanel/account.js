14bd04edac52c87c8632749557189af068246b77
maniraad <maniraad7@gmail.com>
2024-03-28 20:40:14 +0330

    Get And Show User Infos In Input Value

---
+++ frontend/public/js/UPanel/account.js
@@ -0,0 +1,5 @@
+import { getAndShowUserInfos } from "./func/shared.js";
+
+window.addEventListener("load", () => {
+    getAndShowUserInfos();
+});
--- frontend/public/js/UPanel/func/shared.js
+++ frontend/public/js/UPanel/func/shared.js
@@ -34,4 +34,30 @@ const getAndShowUseCourse = async () => {
     });
 };
 
-export { getAndShowUseCourse };
+const getAndShowUserInfos = async () => {
+    const nameInput = document.querySelector('#nameInput');
+    const userNameInput = document.querySelector('#userNameInput');
+    const phoneInput = document.querySelector('#phoneInput');
+    const emailInput = document.querySelector('#emailInput');
+    const passwordInput = document.querySelector('#passwordInput');
+    const showUserName = document.querySelector('.show-userName');
+
+    const res = await fetch(`http://localhost:4000/v1/auth/me`, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    });
+
+    const data = await res.json()
+
+    console.log(res);
+
+    console.log(data);
+    showUserName.innerHTML = data.username
+    nameInput.value = data.name 
+    userNameInput.value = data.username
+    phoneInput.value = data.phone
+    emailInput.value = data.email
+};
+
+export { getAndShowUseCourse, getAndShowUserInfos };
